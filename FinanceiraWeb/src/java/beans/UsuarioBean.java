7854f1f311495aba28f9ac8589ee39fcb59f77d2
lari.rsoares <lari.rsoares@804bf8a3-2906-627d-2319-715ea0a4da3b>
2011-04-20 21:52:41 +0000

    tudo

---
+++ FinanceiraWeb/src/java/beans/UsuarioBean.java
@@ -0,0 +1,47 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package beans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author Igor
+ */
+@ManagedBean(name="userBean")
+@SessionScoped
+public class UsuarioBean {
+
+    /** Creates a new instance of UsuarioBean */
+    private String login;
+    private String password;
+
+    public UsuarioBean() {
+    }
+
+    public UsuarioBean(String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
