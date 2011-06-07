55a1010a3a69bd81a69a9c959fcf451a6644ee2d
igorcrexito <igorcrexito@804bf8a3-2906-627d-2319-715ea0a4da3b>
2011-06-07 05:24:36 +0000

    Alterações no acesso ao BD e já inserção de usuário

--- trunk/FinanceiraWeb/src/java/beans/NovoUsuarioBean.java
+++ trunk/FinanceiraWeb/src/java/beans/NovoUsuarioBean.java
@@ -1,8 +1,14 @@
 package beans;
 
 
+import classes.Usuario;
+import conexao.UsuarioDAO;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.enterprise.context.SessionScoped;
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
 
 
 /**
@@ -18,14 +24,14 @@ public class NovoUsuarioBean {
     private String login;
     private String password;
     private String passwordConfirmar;
-    private int nivelAcesso;
+    private String nivelAcesso;
     private String nome;
     private String erro="";
 
     public NovoUsuarioBean() {
     }
 
-    public NovoUsuarioBean(String login, String password,String passwordConf, int nivelAcesso, String nome) {
+    public NovoUsuarioBean(String login, String password,String passwordConf, String nivelAcesso, String nome) {
         this.nivelAcesso = nivelAcesso;
         this.passwordConfirmar = passwordConf;
         this.nome = nome;
@@ -35,11 +41,49 @@ public class NovoUsuarioBean {
 
 
     public void criaNovoUsuario () {
+        checaTodos();
+        UsuarioDAO userDao = new UsuarioDAO();
+        boolean valid = true;
+        System.out.println(nivelAcesso + "nivel");
+        valid = userDao.checaLoginValido(this.login);
+
+
+        if(valid) {
+            erro= "";
+        } else {
+            erro = "Login de usuário já cadastrado. Cadastre um outro";
+        }
+
+        if (erro.equals(""))
         if (password.equals(passwordConfirmar)) {
             erro = "Usuario inserido com sucesso";
+            checaTodos();
         } else {
             erro = "A senha e confirmação diferem";
+            checaTodos();
         }
+
+        if (erro.equals("Usuario inserido com sucesso")) {
+            userDao.inserirUsuario(login, password, nome, Integer.parseInt(nivelAcesso));
+        }
+;
+    }
+
+    public void checaTodos() {
+        if (this.login.equals("") || this.nome.equals("") || this.password.equals("") || this.passwordConfirmar.equals("")) {
+            erro = "Todos os campos precisam ser preenchidos";
+        } 
+    }
+
+    public String resetaUsuario () {
+        this.passwordConfirmar = "";
+        this.nome = "";
+        this.login = "";
+        this.password = "";
+        this.erro="";
+        this.nivelAcesso="";
+        return "home.xhtml";
+        
     }
 
     public String getErro() {
@@ -58,11 +102,11 @@ public class NovoUsuarioBean {
         this.login = login;
     }
 
-    public int getNivelAcesso() {
+    public String getNivelAcesso() {
         return nivelAcesso;
     }
 
-    public void setNivelAcesso(int nivelAcesso) {
+    public void setNivelAcesso(String nivelAcesso) {
         this.nivelAcesso = nivelAcesso;
     }
 
