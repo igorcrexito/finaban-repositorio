9c5b6169a1a3044682e4c15298f8dc81e4cdde60
igorcrexito@gmail.com <igorcrexito@gmail.com@804bf8a3-2906-627d-2319-715ea0a4da3b>
2011-06-09 02:01:49 +0000

    Classe de mensagem

---
+++ trunk/FinanceiraWeb/src/java/classes/Mensagem.java
@@ -0,0 +1,37 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package classes;
+
+    public class Mensagem {
+
+     private String destino;
+     private String titulo;
+     private String mensagem;
+
+     public String getDestino() {
+     return destino;
+     }
+
+     public void setDestino(String destino) {
+     this.destino = destino;
+     }
+
+     public String getMensagem() {
+     return mensagem;
+     }
+
+     public void setMensagem(String mensagem) {
+     this.mensagem = mensagem;
+     }
+
+     public String getTitulo() {
+     return titulo;
+     }
+
+     public void setTitulo(String titulo) {
+     this.titulo = titulo;
+     }
+    }
